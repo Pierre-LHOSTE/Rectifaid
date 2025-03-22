eae8729ebba40a47c10d384f457af5ece52bf62e
Pierre LHOSTE <vingt-douze@protonmail.com>
2025-03-22 12:15:37 +0100

    fix: improve ExplanationsList key handling

--- src/components/explanation-list/ExplanationsList.tsx
+++ src/components/explanation-list/ExplanationsList.tsx
@@ -1,7 +1,7 @@
 import type { ExplanationType } from "@/types/explanations";
 import "./explanations-list.scss";
-import ExplanationItem from "../explanation-item/ExplanationItem";
 import { Divider } from "antd";
+import ExplanationItem from "../explanation-item/ExplanationItem";
 
 export default function ExplanationsList({
 	explanations,
@@ -11,11 +11,11 @@ export default function ExplanationsList({
 	return (
 		<div id="explanations-list">
 			{explanations.map((explanation, index) => (
-				<>
-					{/* biome-ignore lint/suspicious/noArrayIndexKey: The order doesn't change */}
-					<ExplanationItem key={index} explanation={explanation} />
+				// biome-ignore lint/suspicious/noArrayIndexKey: The order doesn't change
+				<div key={index}>
+					<ExplanationItem explanation={explanation} />
 					{index < explanations.length - 1 && <Divider />}
-				</>
+				</div>
 			))}
 		</div>
 	);
