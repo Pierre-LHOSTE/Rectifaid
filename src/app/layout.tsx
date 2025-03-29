391daefa48d9af08a2fe70db4e06f83975fd9968
Pierre LHOSTE <vingt-douze@protonmail.com>
2025-03-29 21:01:10 +0100

    feat: update layout for safe area insets and adjust theme color in manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./layout.css";
 import type { ReactNode } from "react";
@@ -9,6 +9,19 @@ import "overlayscrollbars/overlayscrollbars.css";
 export const metadata: Metadata = {
 	title: "Rectifaid",
 	description: "A tiny tool to correct your text with AI",
+};
+
+// biome-ignore lint/nursery/useComponentExportOnlyModules: <explanation>
+export const viewport: Viewport = {
+	initialScale: 1,
+	userScalable: false,
+	width: "device-width",
+	height: "device-height",
+	viewportFit: "cover",
+	themeColor: [
+		{ media: "(prefers-color-scheme: dark)", color: "#000000" },
+		{ media: "(prefers-color-scheme: light)", color: "#ffffff" },
+	],
 };
 
 export default function RootLayout({
